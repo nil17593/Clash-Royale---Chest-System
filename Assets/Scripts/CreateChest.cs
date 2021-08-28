62495e0bd9710d7e50622e406f6c54b5f44ded4e
nil17593 <navgekar18@gmail.com>
2021-08-28 18:30:26 +0530

    created UI.

---
+++ Assets/Scripts/CreateChest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateChest : MonoBehaviour
+{
+    [SerializeField] private Button buttonCreateChest;
+
+    void Start()
+    {
+        buttonCreateChest.onClick.AddListener(CreateNewChest);
+    }
+
+    void CreateNewChest()
+    {
+        
+    }
+        
+}
